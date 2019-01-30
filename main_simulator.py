bd82e09ed6a37195ce24844f4b9005f21a655f3d
AdamPiascik <43042327+AdamPiascik@users.noreply.github.com>
2019-01-30 15:43:46 +0000

    added gender check and put code inside a main function

--- main_simulator.py
+++ main_simulator.py
@@ -1,30 +1,39 @@
 import tennisSys
 import tennisSim
 
-# Initialises a simulation system by generating player lists #
-sys = tennisSys.MakeSystem()
+def main():
+    # Initialises a simulation system by generating player lists #
+    sys = tennisSys.MakeSystem()
 
-# Print lists of players and their attributes from the system #
-sys.printLists()
-sys.listPlayerStats("All Players")
+    # Print lists of players and their attributes from the system #
+    # sys.printLists()
+    # sys.listPlayerStats("All Players")
 
-# Choose two players for the simulation from a specified player list #
-# player1 = sys.getPlayer("Andy Murray", "All Players")
-# player2 = sys.getPlayer("Roger Federer", "All Players")
+    # Choose two players for the simulation from a specified player list #
+    player1 = sys.getPlayer("Roger Federer", "All Players")
+    player2 = sys.getPlayer("Juan Martin Del Potro", "All Players")
 
-# Create the simulation environment #
-# current_sim = tennisSim.InitialiseSimulation(player1, player2)
+    # Create the simulation environment if player genders match #
+    if player1.gender == player2.gender:
+        current_sim = tennisSim.InitialiseSimulation(player1, player2)
+    else:
+        print("Simulation creation failed: the genders of the specified players don't match.")
+        return 1
 
-# Print various useful information about the current simulation #
-# current_sim.printPlayers()
-# current_sim.printType()
-# current_sim.printResults()
+    # Print various useful information about the current simulation #
+    # current_sim.printPlayers()
+    # current_sim.printType()
+    # current_sim.printResults()
 
-# Performs a point-playing simulation and stores the win rate of each player #
-# current_sim.resetSim()
-# current_sim.runSimulation("points", 10000)
-# current_sim.runSimulation("games", 10000)
-# current_sim.runSimulation("sets", 10000)
-# current_sim.runSimulation("matches", 100)
-# print(current_sim.win_record)
-# current_sim.printResults()
+    # Performs a point-playing simulation and stores the win rate of each player #
+    current_sim.resetSim()
+    current_sim.runSimulation("points", 1000000)
+    # current_sim.runSimulation("games", 100000)
+    # current_sim.runSimulation("sets", 10000)
+    # current_sim.runSimulation("matches", 100000)
+    # print(current_sim.win_record)
+    current_sim.printResults()
+    return 0
+
+if __name__ == '__main__':
+    main()
